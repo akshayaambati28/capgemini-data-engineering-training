378ab7f19daff2d4845fbc2429dcc4b776cba29b
akshayaambati28 <akshayaambati28@gmail.com>
2026-04-03 21:54:29 +0530

    sql queries updated

--- Week-0/phase-1/sql_queries.sql
+++ Week-0/phase-1/sql_queries.sql
@@ -1,14 +1,21 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("CustomerExample").getOrCreate()
-
-customers = spark.createDataFrame([
-    (1, "Ravi", "Hyderabad", 25),
-    (2, "Sita", "Chennai", 32),
-    (3, "Arun", "Hyderabad", 28)
-], ["customer_id", "customer_name", "city", "age"])
-customers.show()
-customers.filter(customers.city == "Chennai").show()
-customers.filter(customers.age > 25).show()
-customers.select("customer_name", "city").show()
-customers.count()
+CREATE TABLE customers (
+customer_id INT,
+customer_name VARCHAR(50),
+city VARCHAR(50),
+age INT
+);
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', 25),
+(2, 'Sita', 'Chennai', 32),
+(3, 'Arun', 'Hyderabad', 28);
+SELECT * FROM customers;
+SELECT *
+FROM customers
+WHERE city = 'Chennai';
+SELECT *
+FROM customers
+WHERE age > 25;
+SELECT customer_name, city
+FROM customers;
+SELECT COUNT(*) AS total_customers
+FROM customers;
