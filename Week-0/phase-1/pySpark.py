7d43c6ddb1ce3cabad4595081a24225bd6165ddb
akshayaambati28 <akshayaambati28@gmail.com>
2026-04-03 21:57:38 +0530

    guided exercises phase1 completed

--- Week-0/phase-1/pySpark.py
+++ Week-0/phase-1/pySpark.py
@@ -1,1 +1,14 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("CustomerExample").getOrCreate()
+
+customers = spark.createDataFrame([
+    (1, "Ravi", "Hyderabad", 25),
+    (2, "Sita", "Chennai", 32),
+    (3, "Arun", "Hyderabad", 28)
+], ["customer_id", "customer_name", "city", "age"])
+customers.show()
+customers.filter(customers.city == "Chennai").show()
+customers.filter(customers.age > 25).show()
+customers.select("customer_name", "city").show()
+customers.count()
